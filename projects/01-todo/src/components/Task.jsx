bf6c6bb89208d9ebe8451f9e1680c8f8e92eb136
Markus <marcosbh2002bh@gmail.com>
2024-05-14 00:06:52 +0200

    Ahora toma los valores del formulario

--- projects/01-todo/src/components/Task.jsx
+++ projects/01-todo/src/components/Task.jsx
@@ -3,7 +3,9 @@ import PropTypes from "prop-types"
 export function Task({ title, children }) {
     return (
         <article
-            className={"flex w-2/3 flex-col gap-3 border bg-neutral-700 p-1 "}
+            className={
+                "flex w-2/3 flex-col gap-3 border bg-slate-700 p-1 dark:bg-neutral-700"
+            }
         >
             <h2 className={"font-bold"}>{title}</h2>
             <p>{children}</p>
--- projects/01-todo/src/components/TaskCreator.jsx
+++ projects/01-todo/src/components/TaskCreator.jsx
@@ -1,10 +1,52 @@
+import { useForm } from "react-hook-form"
+
 export function TaskCreator() {
+    const { register, handleSubmit } = useForm()
+
+    function sendForm(data) {
+        const task = {
+            title: data.title,
+            description: data.description,
+        }
+
+        console.log(task)
+    }
+
     return (
-        <form>
-            <label>
-                Titulo de la tarea
-                <input type={"text"} />
-            </label>
-        </form>
+        <fieldset className={"mx-3 border p-3"}>
+            <legend>Creador de tareas</legend>
+            <form
+                className={"mx-3 flex flex-col items-center gap-5"}
+                onSubmit={handleSubmit(sendForm)}
+            >
+                <label className={"flex items-center gap-2"}>
+                    Titulo de la tarea:
+                    <input
+                        type={"text"}
+                        name={"title"}
+                        {...register("title")}
+                        className={"text-black"}
+                    />
+                </label>
+
+                <label className={"flex items-center gap-2"}>
+                    Cuerpo de la tarea:
+                    <textarea
+                        name={"description"}
+                        className={"text-black"}
+                        {...register("description")}
+                    />
+                </label>
+
+                <button
+                    type="submit"
+                    className={
+                        "w-2/3 border transition-colors hover:bg-orange-400 dark:hover:bg-neutral-400"
+                    }
+                >
+                    Enviar
+                </button>
+            </form>
+        </fieldset>
     )
 }
