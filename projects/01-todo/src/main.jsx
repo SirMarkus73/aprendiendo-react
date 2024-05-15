2d9a4bc73ef0eec4e81cd54b1134453860b0b323
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-15 13:18:32 +0200

    Eliminados imports innecesarios

--- projects/01-todo/src/main.jsx
+++ projects/01-todo/src/main.jsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react"
+import React from "react"
 import ReactDOM from "react-dom/client"
 
-import { TaskCreator } from "./components/TaskCreator.jsx"
-import { TaskList } from "./components/TaskList.jsx"
-import { getTasks } from "./logic.js"
-
-import "./index.css"
 import { App } from "./App.jsx"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
