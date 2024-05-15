d5b95c9b46b6e72da14d33dc11aba3eeeaf722ba
Markus <marcosbh2002bh@gmail.com>
2024-05-15 19:28:31 +0200

    Subida inicial del proyecto

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <>
+      <h1
+        className={
+          "flex flex-row bg-orange-400 text-orange-700 caret-amber-500 justify-center text-2xl"
+        }
+      >
+        Test
+      </h1>
+    </>
+  );
+}
