d54a961602ed663e6309d156ce2e5070d6d139d8
Markus <marcosbh2002bh@gmail.com>
2024-05-15 20:54:33 +0200

    App completada

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,13 +1,51 @@
+import { useEffect, useState } from "react";
+
 export function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const hiddenBallClass = enabled ? "" : "hidden";
+  useEffect(() => {
+    console.log("efecto ", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+      document.body.classList.add("no-cursor");
+    }
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+      document.body.classList.remove("no-cursor");
+      setPosition({ x: 0, y: 0 });
+    };
+  }, [enabled]);
+
   return (
-    <>
-      <h1
+    <main className={"grid place-content-center h-screen text-center"}>
+      <div
         className={
-          "flex flex-row bg-orange-400 text-orange-700 caret-amber-500 justify-center text-2xl"
+          "absolute bg-orange-400 rounded-full opacity-75 pointer-events-none left-[-25px] top-[-25px] w-[50px] h-[50px] " +
+          hiddenBallClass
         }
+        style={{
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <h1>Proyecto 3</h1>
+      <button
+        className={
+          "bg-neutral-600 border border-neutral-800 rounded-xl p-3 hover:bg-neutral-700 transition-colors"
+        }
+        onClick={() => {
+          setEnabled(!enabled);
+        }}
       >
-        Test
-      </h1>
-    </>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </main>
   );
 }
